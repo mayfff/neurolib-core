f8609f76e0feb3cd16c178d7a181a950aeff371a
Danylo Zakrevskyi <zakrevsky.d.s@gmail.com>
2026-04-23 16:04:06 +0300

    init commit

---
+++ src/main/java/kpi/zakrevskyi/neurolib/NeurolibApplication.java
@@ -0,0 +1,13 @@
+package kpi.zakrevskyi.neurolib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeurolibApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NeurolibApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kpi/zakrevskyi/neurolib/NeurolibApplicationTests.java
@@ -0,0 +1,13 @@
+package kpi.zakrevskyi.neurolib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeurolibApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
